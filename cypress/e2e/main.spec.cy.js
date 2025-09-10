e7653d9957a85c22fd3475af14e3f75cb4cd18cb
jonas <jonascamposk7ng.gmail.com>
2025-09-09 22:45:36 -0300

    primeiro git clone

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -2,7 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    cy.get('[data-qa="comment-input"]').first().type('Test 1234')
     cy.get('[data-qa="publish-button"]').first().click()
     cy.get('[data-qa="comment-text"]').contains('Test 123')
     cy.get('[data-qa="comment-text"]').each(($element) => {
